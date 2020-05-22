1734de522b46bf63e01891f4ee524f63fb31c5b3
kurt <tomashq@gmail.com>
2020-05-22 13:41:37 +0200

    temporary additional logging to troubleshoot issues in staging only

--- api/_lib/logger.js
+++ api/_lib/logger.js
@@ -14,7 +14,7 @@ const client = new Client({
 
 function createLogger(loggerName) {
     const logger = winston.createLogger({
-        level: 'debug',
+        level: 'info',
         format: winston.format.combine(
             winston.format.timestamp({
                 format: 'YYYY-MM-DD HH:mm:ss'
@@ -30,8 +30,8 @@ function createLogger(loggerName) {
             // - Write to all logs with level `info` and below to `combined.log`
             // - Write all logs error (and below) to `error.log`.
             //
-            // new winston.transports.File({filename: 'error.log', level: 'error'}),
-            // new winston.transports.File({filename: 'combined.log'})
+            new winston.transports.File({filename: 'error.log', level: 'error'}),
+            new winston.transports.File({filename: 'combined.log'}),
             // new Elasticsearch({level: 'debug',indexPrefix:'ota-', client:client}),
             new winston.transports.Console({format: winston.format.simple()})
         ]
