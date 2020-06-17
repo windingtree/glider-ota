e71dcea6a240bf69acb7945b5dda60b1fafe7d0c
Mathieu TAHON <mathieu.tahon@noath.net>
2020-06-17 15:29:19 +0200

    Added default configuration variables and mapping

--- api/_lib/config.js
+++ api/_lib/config.js
@@ -15,61 +15,78 @@ const determineEnviroment = () => {
     }
 }
 const enviroment = determineEnviroment();
+
+// Get an an environment variable
+const getConfigKey = (key) => {
+    // Return environment specific variable if any
+    const envKey = `${enviroment.toUpperCase()}_${key}`;
+    if(process.env.hasOwnProperty(envKey)) {
+      return process.env[envKey];
+    }
   
-const GLIDER_BASEURL = process.env.GLIDER_BASEURL || `https://${enviroment}.b2b.glider.travel/api/v1`;
+    // Return variable key
+    if(process.env.hasOwnProperty(key)) {
+      return process.env[key];
+    }
+  
+    // Config key does not exist
+    return undefined;
+};
+  
+const GLIDER_BASEURL = getConfigKey('GLIDER_BASEURL') || `https://${enviroment}.b2b.glider.travel/api/v1`;
 
 const GLIDER_CONFIG =
     {
-        GLIDER_TOKEN: process.env.GLIDER_JWT,
+        GLIDER_TOKEN: getConfigKey('GLIDER_JWT'),
         SEARCH_OFFERS_URL: GLIDER_BASEURL + "/offers/search",
         CREATE_WITH_OFFER_URL: GLIDER_BASEURL + "/orders/createWithOffer",
         SEATMAP_URL: GLIDER_BASEURL + "/offers/{offerId}/seatmap",
         REPRICE_OFFER_URL: GLIDER_BASEURL + "/offers/{offerId}/price",
         FULFILL_URL: GLIDER_BASEURL + "/orders/{orderId}/fulfill",
-        ORGID: process.env.GLIDER_ORGID || "0x71cd1781a3082f33d2521ac8290c9d4b3b3b116e4e8548a4914b71a1f7201da0",
+        ORGID: getConfigKey('GLIDER_ORGID') || "0x71cd1781a3082f33d2521ac8290c9d4b3b3b116e4e8548a4914b71a1f7201da0",
     };
 
 const ORGID = {
-    OTA_ORGID: process.env.OTA_ORGID || "0x08fffc14df93a305f1bbc2ee26171885fee80181445d8923ab84892953c093f2",
+    OTA_ORGID: getConfigKey('OTA_ORGID') || "0x08fffc14df93a305f1bbc2ee26171885fee80181445d8923ab84892953c093f2",
 }
 
-const SIMARD_BASEURL = process.env.SIMARD_BASEURL || `https://${enviroment}.api.simard.io/api/v1`;
+const SIMARD_BASEURL = getConfigKey('SIMARD_BASEURL') || `https://${enviroment}.api.simard.io/api/v1`;
 
 const SIMARD_CONFIG =
     {
-        SIMARD_TOKEN: process.env.SIMARD_JWT,
+        SIMARD_TOKEN: getConfigKey('SIMARD_JWT'),
         GUARANTEES_URL: SIMARD_BASEURL + "/balances/guarantees",
         CREATE_WITH_OFFER_URL: SIMARD_BASEURL + "/orders/createWithOffer",
         SIMULATE_DEPOSIT_URL: SIMARD_BASEURL + "/balances/simulateDeposit",
-        ORGID: process.env.SIMARD_ORGID || "0x5e6994f76764ceb42c476a2505065a6170178a24c03d81c9f372563830001171",
+        ORGID: getConfigKey('SIMARD_ORGID') || "0x5e6994f76764ceb42c476a2505065a6170178a24c03d81c9f372563830001171",
         DEPOSIT_EXPIRY_DAYS:14,
     };
 
 
 const REDIS_CONFIG =
     {
-        REDIS_PORT: (process.env.REDIS_PORT && parseInt(process.env.REDIS_PORT)) || 14563,
-        REDIS_HOST: process.env.REDIS_HOST,
-        REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+        REDIS_PORT: (getConfigKey('REDIS_PORT') && parseInt(getConfigKey('REDIS_PORT'))) || 14563,
+        REDIS_HOST: getConfigKey('REDIS_HOST'),
+        REDIS_PASSWORD: getConfigKey('REDIS_PASSWORD'),
         SESSION_TTL_IN_SECS: 60 * 60,
     };
 
 const MONGO_CONFIG =
     {
-        URL: process.env.MONGO_URL,
-        DBNAME: process.env.MONGO_DBNAME,
+        URL: getConfigKey('MONGO_URL'),
+        DBNAME: getConfigKey('MONGO_DBNAME'),
     };
 
 const STRIPE_CONFIG =
     {
-        PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
-        SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-        WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
-        BYPASS_WEBHOOK_SIGNATURE_CHECK: (process.env.STRIPE_BYPASS_WEBHOOK_SIGNATURE_CHECK === "yes"),
+        PUBLISHABLE_KEY: getConfigKey('STRIPE_PUBLISHABLE_KEY'),
+        SECRET_KEY: getConfigKey('STRIPE_SECRET_KEY'),
+        WEBHOOK_SECRET: getConfigKey('STRIPE_WEBHOOK_SECRET'),
+        BYPASS_WEBHOOK_SIGNATURE_CHECK: (getConfigKey('STRIPE_BYPASS_WEBHOOK_SIGNATURE_CHECK') === "yes"),
     };
 const ELASTIC_CONFIG =
     {
-        URL: process.env.ELASTIC_URL,
+        URL: getConfigKey('ELASTIC_URL'),
     };
 
 module.exports = {
