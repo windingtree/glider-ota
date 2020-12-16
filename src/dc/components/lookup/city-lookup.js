25558c5ab6e2842b6426f93851d8dcdf1a681fb6
Kostiantyn Smyrnov <kostysh@gmail.com>
2020-12-16 16:13:21 +0200

    added spinner to the hotels sities search

--- src/dc/components/lookup/city-lookup.js
+++ src/dc/components/lookup/city-lookup.js
@@ -1,19 +1,46 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import LookupField from "./components/lookup-field";
 import {fetchGet} from "../../../utils/api-utils";
 
 
-export function CityLookup({initialLocation, onSelectedLocationChange, placeHolder, label, localstorageKey}) {
+export function CityLookup(props) {
+    const {
+        initialLocation,
+        onSelectedLocationChange,
+        placeHolder,
+        label,
+        localstorageKey
+    } = props;
     const [searchResults, setSearchResults] = useState([]);
+    const [isLoading, setLoading] = useState(false);
+    const [queryPromise, setQueryPromise] = useState(null);
+
+    useEffect(() => {
+        let queryProcess = queryPromise;
+        if (queryProcess && typeof queryProcess.then === 'function') {
+            queryProcess
+                .then(response => {
+                    let airports = convertResponse(response.results);
+                    setLoading(false);
+                    setSearchResults(airports);
+                })
+                .catch(error => {
+                    setLoading(false);
+                    console.log('Failed to search for airports', error);
+                });
+        }
+        return () => {
+            queryProcess = undefined;
+        };
+    }, [queryPromise]);
 
     async function onQueryEntered(searchQuery) {
-        let results = fetchGet('/api/lookup/citySearch', {searchquery: searchQuery});
-        results.then((response) => {
-            let airports = convertResponse(response.results);
-            setSearchResults(airports);
-        }).catch(err => {
-            console.error("Failed to search for locations", err)
-        })
+        setLoading(true);
+        setQueryPromise(
+            fetchGet('/api/lookup/citySearch', {
+                searchquery: searchQuery
+            })
+        );
     }
 
     function convertResponse(airports) {
@@ -28,8 +55,16 @@ export function CityLookup({initialLocation, onSelectedLocationChange, placeHold
     }
 
     return (
-        <LookupField initialLocation={initialLocation} onSelectedLocationChange={onSelectedLocationChange}
-                     placeHolder={placeHolder} onQueryEntered={onQueryEntered} locations={searchResults} label={label} localstorageKey={localstorageKey}/>
+        <LookupField
+            initialLocation={initialLocation}
+            onSelectedLocationChange={onSelectedLocationChange}
+            placeHolder={placeHolder}
+            onQueryEntered={onQueryEntered}
+            locations={searchResults}
+            label={label}
+            localstorageKey={localstorageKey}
+            loading={isLoading}
+        />
     )
 }
 
