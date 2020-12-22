3ac849685798720ee8feddc1ef91ead22f73d66a
Mathieu TAHON <mathieu.tahon@noath.net>
2020-12-22 13:16:54 +0100

    Fixed icons

--- src/dc/components/accommodation-blocks/lodging-info.js
+++ src/dc/components/accommodation-blocks/lodging-info.js
@@ -1,4 +1,6 @@
 import {FaBed} from "react-icons/fa";
+import {IconHotelBed} from '../icons/icons';
+
 import style from "./lodging-info.module.scss"
 import React from 'react';
 import {format, parseISO} from "date-fns";
@@ -8,7 +10,7 @@ import {IconContext} from "react-icons";
 export const LodgingInfo = ({checkInDate, checkOutDate, cityName}) => {
     return (
         <div className={style.adBox}>
-            <div className={style.adIcon}><IconContext.Provider value={{ className: "icon-primary-color" }}><FaBed/></IconContext.Provider></div>
+            <div className={style.adIcon}><IconHotelBed/></div>
             <div className={style.adDetails}>
                 <div className={style.adDate}>
                     {checkInDate && safeDateFormat(checkInDate, 'dd MMM')}
