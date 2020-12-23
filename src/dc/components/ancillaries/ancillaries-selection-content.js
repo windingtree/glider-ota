afd5bb1d689065c99fc53f26e7c1fe5492e677e0
kurt <tomashq@gmail.com>
2020-12-23 02:48:58 +0100

    WTR-732, WTR-724 increased padding on pax page and ancillary selection

--- src/dc/components/ancillaries/ancillaries-selection-content.js
+++ src/dc/components/ancillaries/ancillaries-selection-content.js
@@ -147,13 +147,13 @@ export function FareFamilies({tripRates, selectedOffer, onSelectedOfferChange}) 
 const NaviButtons = ({prevEnabled, nextEnabled, onPrev, onNext})=>{
     return(
         <Row className={'pt-3 pb-5'}>
-            <Col sm={4}>
+            <Col xs={5}>
                 <Button className={'btn-block'} variant="outline-primary"  disabled={prevEnabled===false} onClick={onPrev}>Back</Button>
             </Col>
-            <Col sm={4}>
+            <Col xs={2}>
                 <div className={'pt-2'}></div>
             </Col>
-            <Col sm={4}>
+            <Col xs={5}>
                 <Button className={'btn-block'} variant="primary"  disabled={nextEnabled===false} onClick={onNext}>Proceed</Button>
             </Col>
         </Row>
