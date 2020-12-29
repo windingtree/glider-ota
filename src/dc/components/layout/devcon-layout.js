14fa46f0a94c720e445fc3f196971d4273563857
Maksim Izmaylov <me@kvakes.com>
2020-12-29 14:26:08 -0300

    Add the ability to customize each page that extends DevconLayout with CSS without leaking CSS into global scope

--- src/dc/components/layout/devcon-layout.js
+++ src/dc/components/layout/devcon-layout.js
@@ -2,11 +2,13 @@ import React from "react";
 import style from "./devcon-layout.module.scss";
 
 
-export default function DevConLayout({children}) {
+export default function DevConLayout(props) {
     return (
         <div className={style.outerPageWrapper}>
             <div className={style.innerPageWrapper}>
-                {children}
+                <div className={props.pageClass ? " " + props.pageClass : ""}>
+                    {props.children}
+                </div>
             </div>
         </div>)
 }
