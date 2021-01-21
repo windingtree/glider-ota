c84cf1889b064ffe780dd57fe7deb2dae16ff478
kurt <tomashq@gmail.com>
2021-01-22 00:05:23 +0100

    improved city search

--- api/_lib/lookup-manager.js
+++ api/_lib/lookup-manager.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const {MONGO_CONFIG} = require('./config');
-const {createLogger} = require('./logger');
-const logger = createLogger('dao');
-const SEARCH_CONFIG = {
+const AIRPORT_SEARCH_CONFIG = {
     BY_CITY_NAME: {
         MIN_QUERY_LENGTH: 3,
         WEIGHT: 10
@@ -21,17 +19,29 @@ const SEARCH_CONFIG = {
     }
 }
 
+const CITY_SEARCH_CONFIG = {
+    BY_CITY_NAME: {
+        MIN_QUERY_LENGTH: 3,
+        WEIGHT: 30
+    },
+    BY_ASCII_NAME: {
+        MIN_QUERY_LENGTH: 3,
+        WEIGHT: 20
+    },
+    BY_ALTERNATE_NAMES: {
+        MIN_QUERY_LENGTH: 3,
+        WEIGHT: 10
+    }
+}
 const LOOKUP_CONFIG = {
     MIN_QUERY_LENGTH: 3,
     MAX_RESULTS: 30
 }
 
 
-console.log(`Using DB:${MONGO_CONFIG.URL}`);
-
 mongoose.connect(MONGO_CONFIG.URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
-
+//Table which stores airports (needed to populate airport lookup field)
 const Airports = mongoose.model('airports', mongoose.Schema({
     city_name: String,
     city_code: String,
@@ -41,11 +51,33 @@ const Airports = mongoose.model('airports', mongoose.Schema({
     type: String,
     country_name: String,
     timezone: String,
-    pagerank: Number,
+    pagerank: Number,   //how popular given airport is
     belongs_to_metropolitan:{type: Boolean, default: false}
 }), 'airportscurated');
 
+//SAMPLE AIRPORT RECORDS
+// { "city_name" : "New York", "city_code" : "NYC", "country_code" : "US", "airport_name" : "Newark Liberty Intl", "airport_iata_code" : "EWR", "type" : "AIRPORT", "country_name" : "United States", "timezone" : "America/New_York" }
+// { "city_name" : "New York", "city_code" : "NYC", "country_code" : "US", "airport_name" : "Stewart International", "airport_iata_code" : "SWF", "type" : "AIRPORT", "country_name" : "United States", "timezone" : "America/New_York" }
 
+//Table which stores cities (needed to populate cities lookup field in hotel search)
+const Cities = mongoose.model('cities', mongoose.Schema({
+    city_name: String,
+    country_code: String,
+    country_name: String,
+    asciiname: String,
+    alternatenames: String,
+    latitude: String,
+    longitude: String,
+    population: Number, //number of people in a given city
+}), 'citiescurated');
+
+//SAMPLE RECORD
+/* {
+    "city_name" : "Luanda","country_code" : "AO","latitude" : "-8.83682","longitude" : "13.23432","population" : 2776168,"asciiname" : "Luanda","country_name" : "Angola",
+    "alternatenames" : "LAD,Loanda,Louanda,Louanta,Luand,Luanda,Luandae,Luando,Lwanda,Lúanda,Saint Paul de Loanda,Sao Paolo de Loanda,Sao Paulo da Assuncao de Luanda,Sao Paulo de Loanda,Sao Paulo de Luanda,St Paul de Loanda,São Paolo de Loanda,São Paulo da Assunção de Luanda,São Paulo de Loanda,São Paulo de Luanda,lu'anda,luanda,luo an da,luvanta,luxanda,luyanda,lwanda,ruanda,Λουάντα,Луандæ,Луанда,Լուանդա,לואנדה,לואנדע,لوآندا,لواندا,لونڈا,लुआंडा,लुआण्डा,লুয়ান্ডা,ਲੁਆਂਦਾ,லுவாண்டா,ลูอันดา,ལའུན་ཌ།,ლუანდა,ሏንዳ,ルアンダ,罗安达,루안다",
+}*/
+
+/////////////// GENERIC FUNCTIONS ////////////////////
 const isQueryLongerOrEqualThan = (query, length) => {
     if (!query)
         return false;
@@ -60,6 +92,21 @@ const decorateRecordWithWeight = (records, weight) => {
     })
     return records;
 }
+const multipleComparators = (comparators) =>{
+    const compare=(A,B)=>{
+        for(let comparator of comparators){
+            let result = comparator(A,B);
+            if(result !== 0)
+                return result;
+        }
+        return 0;
+    }
+    return compare;
+}
+
+
+/////////////// AIRPORT SEARCH HELPERS ////////////////////
+
 /**
  * Wrapper on airports query
  * @param query
@@ -79,131 +126,40 @@ const printResults = (message, airports) => {
     })
 }
 
-const searchAirportByString = async query => {
-    const dbQuery = [
-        {
-            $match: {
-                $or: [
-                    {
-                        'airport_iata_code': {
-                            '$regex': `^${query}`,
-                            '$options': 'i'
-                        }
-                    },
-                    {
-                        'city_name': {
-                            '$regex': `^${query}`,
-                            '$options': 'i'
-                        }
-                    },
-                    {
-                        'city_code': {
-                            '$regex': `^${query}`,
-                            '$options': 'i'
-                        }
-                    },
-                    {
-                        'airport_name': {
-                            '$regex': `^${query}`,
-                            '$options': 'i'
-                        }
-                    }
-                ]
-            }
-        },
-        {
-            $group: {
-                '_id': {
-                    'airport_iata_code': '$airport_iata_code',
-                    'type': '$type'
-                },
-                'city_name': {
-                    $first: '$city_name'
-                },
-                'city_code': {
-                    $first: '$city_code'
-                },
-                'country_code': {
-                    $first: '$country_code'
-                },
-                'airport_name': {
-                    $first: '$airport_name'
-                },
-                'country_name': {
-                    $first: '$country_name'
-                },
-                'timezone': {
-                    $first: '$timezone'
-                },
-            }
-        },
-        {
-            $project: {
-                '_id': 0,
-                'airport_iata_code': '$_id.airport_iata_code',
-                'type': '$_id.type',
-                'city_name': 1,
-                'city_code': 1,
-                'country_code': 1,
-                'airport_name': 1,
-                'country_name': 1,
-                'timezone': 1
-            }
-        },
-        {
-            $sort: {
-                'city_name': 1
-            }
-        }
-    ];
-    return Airports.aggregate(dbQuery);
-};
-
-const searchByExactAirportCode = async (airportCode) => {
-    if (!isQueryLongerOrEqualThan(airportCode, SEARCH_CONFIG.BY_AIRPORT_CODE.MIN_QUERY_LENGTH))
+const airportSearchByExactAirportCode = async (airportCode) => {
+    if (!isQueryLongerOrEqualThan(airportCode, AIRPORT_SEARCH_CONFIG.BY_AIRPORT_CODE.MIN_QUERY_LENGTH))
         return [];
     let results = await searchAirports({'airport_iata_code': {'$regex': `^${airportCode}`, '$options': 'i'}},{pagerank:-1,city_name:1 });
-    results = decorateRecordWithWeight(results, SEARCH_CONFIG.BY_AIRPORT_NAME.WEIGHT);
-    printResults(`searchByExactAirportCode(${airportCode})==>${results.length}`, results);
+    results = decorateRecordWithWeight(results, AIRPORT_SEARCH_CONFIG.BY_AIRPORT_NAME.WEIGHT);
+    // printResults(`searchByExactAirportCode(${airportCode})==>${results.length}`, results);
     return results;
 }
-const searchByCityName = async (cityName) => {
-    if (!isQueryLongerOrEqualThan(cityName, SEARCH_CONFIG.BY_CITY_NAME.MIN_QUERY_LENGTH))
+const airportSearchByCityName = async (cityName) => {
+    if (!isQueryLongerOrEqualThan(cityName, AIRPORT_SEARCH_CONFIG.BY_CITY_NAME.MIN_QUERY_LENGTH))
         return [];
     let results = await searchAirports({'city_name': {'$regex': `^${cityName}`, '$options': 'i'}},{pagerank:-1,city_name:1 });
-    results = decorateRecordWithWeight(results, SEARCH_CONFIG.BY_CITY_NAME.WEIGHT);
-    printResults(`searchByCityName(${cityName})==>${results.length}`, results);
+    results = decorateRecordWithWeight(results, AIRPORT_SEARCH_CONFIG.BY_CITY_NAME.WEIGHT);
+    // printResults(`searchByCityName(${cityName})==>${results.length}`, results);
     return results;
 }
-const searchByCityCode = async (cityCode) => {
-    if (!isQueryLongerOrEqualThan(cityCode, SEARCH_CONFIG.BY_CITY_CODE.MIN_QUERY_LENGTH))
+const airportSearchByCityCode = async (cityCode) => {
+    if (!isQueryLongerOrEqualThan(cityCode, AIRPORT_SEARCH_CONFIG.BY_CITY_CODE.MIN_QUERY_LENGTH))
         return [];
     let results = await searchAirports({'city_code': {'$regex': `^${cityCode}`, '$options': 'i'}},{pagerank:-1,city_name:1 });
-    results =  decorateRecordWithWeight(results, SEARCH_CONFIG.BY_CITY_CODE.WEIGHT);
-    printResults(`searchByCityCode(${cityCode})==>${results.length}`, results);
+    results =  decorateRecordWithWeight(results, AIRPORT_SEARCH_CONFIG.BY_CITY_CODE.WEIGHT);
+    // printResults(`searchByCityCode(${cityCode})==>${results.length}`, results);
     return results;
 }
 
-const searchByAirportName = async (airportName) => {
-    if (!isQueryLongerOrEqualThan(airportName, SEARCH_CONFIG.BY_AIRPORT_NAME.MIN_QUERY_LENGTH))
+const airportSearchByAirportName = async (airportName) => {
+    if (!isQueryLongerOrEqualThan(airportName, AIRPORT_SEARCH_CONFIG.BY_AIRPORT_NAME.MIN_QUERY_LENGTH))
         return [];
     let results = await searchAirports({'airport_name': {'$regex': `^${airportName}`, '$options': 'i'}},{pagerank:-1,city_name:1 });
-    results =  decorateRecordWithWeight(results, SEARCH_CONFIG.BY_AIRPORT_NAME.WEIGHT);
+    results =  decorateRecordWithWeight(results, AIRPORT_SEARCH_CONFIG.BY_AIRPORT_NAME.WEIGHT);
     printResults(`searchByAirportName(${airportName})==>${results.length}`, results);
     return results;
 }
 
-const multipleComparators = (comparators) =>{
-    const compare=(A,B)=>{
-        for(let comparator of comparators){
-            let result = comparator(A,B);
-            if(result != 0)
-                return result;
-        }
-        return 0;
-    }
-    return compare;
-}
 const byPageRankComparator = (A, B) => {
     let rankA=Number(A.pagerank);
     let rankB=Number(B.pagerank);
@@ -214,45 +170,7 @@ const byPageRankComparator = (A, B) => {
     return rankB-rankA;
 }
 
-
-const byWeightComparator = (A, B) => {
-    let weightA = A.weight || 0;
-    let weightB = B.weight || 0;
-    return weightB - weightA;
-}
-const byTypeComparator = (A, B) => {
-    let typeA=A.type || 'AIRPORT';
-    let typeB=B.type || 'AIRPORT';
-    if(typeA !== typeB){
-        return typeB.localeCompare(typeA)
-    }
-}
-const byAirportNameComparator = (A, B) => {
-    let nameA = A.airport_name || '';
-    let nameB = B.airport_name || '';
-    return nameA.localeCompare(nameB)
-}
-
-
-
-const sortResults = (results) => {
-    const comparator = (A, B) => {
-        let typeA=A.type || 'AIRPORT';
-        let typeB=B.type || 'AIRPORT';
-        if(typeA !== typeB){
-            return typeB.localeCompare(typeA)
-        }
-        let weightA = A.weight || 0;
-        let weightB = B.weight || 0;
-        if (weightA !== weightB)
-            return weightB - weightA;
-        let nameA = A.airport_name || '';
-        let nameB = B.airport_name || '';
-        return nameA.localeCompare(nameB)
-    }
-    return results.sort(comparator);
-}
-const removeDupes = (airports) => {
+const removeDupeAirports = (airports) => {
     const isSame = (a,b) =>{
         return ((a.airport_iata_code === b.airport_iata_code) && (a.type === b.type));
     }
@@ -270,20 +188,11 @@ const getMetropolitanArea = async (cityCode) => {
     return undefined;
 }
 
-// { "city_name" : "New York", "city_code" : "NYC", "country_code" : "US", "airport_name" : "Newark Liberty Intl", "airport_iata_code" : "EWR", "type" : "AIRPORT", "country_name" : "United States", "timezone" : "America/New_York" }
-// { "city_name" : "New York", "city_code" : "NYC", "country_code" : "US", "airport_name" : "Stewart International", "airport_iata_code" : "SWF", "type" : "AIRPORT", "country_name" : "United States", "timezone" : "America/New_York" }
-// { "city_name" : "New York", "city_code" : "NYC", "country_code" : "US", "airport_name" : "LaGuardia", "airport_iata_code" : "LGA", "type" : "AIRPORT", "country_name" : "United States", "timezone" : "America/New_York" }
-// { "city_name" : "New York", "city_code" : "NYC", "country_code" : "US", "airport_name" : "John F Kennedy Intl", "airport_iata_code" : "JFK", "type" : "AIRPORT", "country_name" : "United States", "timezone" : "America/New_York" }
-// { "city_name" : "New York", "city_code" : "NYC", "country_code" : "US", "airport_name" : "Metropolitan Area", "airport_iata_code" : "NYC", "type" : "METROPOLITAN", "country_name" : "United States" }
-// { "city_name" : "New York", "city_code" : "NYC", "country_code" : "US", "airport_name" : "Skyports SPB", "airport_iata_code" : "NYS", "type" : "AIRPORT", "country_name" : "United States" }
-
-const airportLookupNew = async query => {
-    if (!query || query.length < LOOKUP_CONFIG.MIN_QUERY_LENGTH) {
-        return [];
-    }
-    return searchAirportByString(query);
-};
-
+/**
+ * Main function that will search for an airport using specified search query
+ * @param name
+ * @returns {Promise<*|[]|*[]>}
+ */
 const airportLookup = async (name) => {
     if (!isQueryLongerOrEqualThan(name, LOOKUP_CONFIG.MIN_QUERY_LENGTH)) {
         return [];
@@ -291,10 +200,10 @@ const airportLookup = async (name) => {
 
     //this should be optimized to avoid multiple calls - use one or two queries instead
     const promises = [
-        searchByExactAirportCode(name),
-        searchByCityCode(name),
-        searchByAirportName(name),
-        searchByCityName(name)];
+        airportSearchByExactAirportCode(name),
+        airportSearchByCityCode(name),
+        airportSearchByAirportName(name),
+        airportSearchByCityName(name)];
     const results = await Promise.all(promises);
     let records = [];
     results.forEach(airports => {
@@ -305,7 +214,7 @@ const airportLookup = async (name) => {
 
     records = records.sort(multipleComparators([byPageRankComparator]));
 
-    records = removeDupes(records);
+    records = removeDupeAirports(records);
     const MAX_LEN = 5;
     if (records.length > MAX_LEN) {
         records = records.splice(0, MAX_LEN)
@@ -335,12 +244,93 @@ const airportLookup = async (name) => {
                 finalResults.push(...airports)
             }
     }
-    finalResults = removeDupes(finalResults);
+    finalResults = removeDupeAirports(finalResults);
     return finalResults;
 }
 
+
+/////////////// CITY SEARCH HELPERS ////////////////////
+
+/**
+* Wrapper on cities query
+* @param query
+* @returns {Promise<*>}
+*/
+const searchCities = async (query, orderBy) => {
+    let q = Cities.find(query)
+    if (orderBy)
+        q = q.sort(orderBy)
+    let results = await q.exec();
+    return results;
+}
+
+const citySearchByCityName = async (cityName) => {
+    if (!isQueryLongerOrEqualThan(cityName, CITY_SEARCH_CONFIG.BY_CITY_NAME.MIN_QUERY_LENGTH))
+        return [];
+    let results = await searchCities({'city_name': {'$regex': `^${cityName}`, '$options': 'i'}},{population:-1,city_name:1 });
+    results = decorateRecordWithWeight(results, CITY_SEARCH_CONFIG.BY_CITY_NAME.WEIGHT);
+    return results;
+}
+
+const citySearchByAsciiName = async (cityName) => {
+    if (!isQueryLongerOrEqualThan(cityName, CITY_SEARCH_CONFIG.BY_ASCII_NAME.MIN_QUERY_LENGTH))
+        return [];
+    let results = await searchCities({'asciiname': {'$regex': `^${cityName}`, '$options': 'i'}},{population:-1,city_name:1 });
+    results = decorateRecordWithWeight(results, CITY_SEARCH_CONFIG.BY_CITY_NAME.WEIGHT);
+    return results;
+}
+
+const byPopulationRankComparator = (A, B) => {
+    let rankA=Number(A.population);
+    let rankB=Number(B.population);
+    if(isNaN(rankA))
+        rankA=0;
+    if(isNaN(rankB))
+        rankB=0;
+    return rankB-rankA;
+}
+
+
+const removeDupeCities = (cities) => {
+    const isSame = (a,b) =>{
+        return ((a.city_name === b.city_name));
+    }
+    let uniques = cities.filter((v,i,a)=>a.findIndex(t=>isSame(t,v))===i)
+    return uniques;
+}
+
+const cityLookup = async (name) => {
+    if (!isQueryLongerOrEqualThan(name, LOOKUP_CONFIG.MIN_QUERY_LENGTH)) {
+        return [];
+    }
+
+    //this should be optimized to avoid multiple calls - use one or two queries instead
+    const promises = [
+        citySearchByCityName(name),
+        citySearchByAsciiName(name)];
+    const results = await Promise.all(promises);
+    let records = [];
+    results.forEach(cities => {
+        cities.forEach(airport => {
+            records.push(airport)
+        })
+    })
+
+    records = records.sort(multipleComparators([byPopulationRankComparator]));
+
+    records = removeDupeCities(records);
+    const MAX_LEN = 5;
+    if (records.length > MAX_LEN) {
+        records = records.splice(0, MAX_LEN)
+    }
+    return records;
+}
+
+
+
 module.exports = {
-    searchByExactAirportCode, searchAirports, searchByCityName, searchByAirportName, airportLookup
+    searchByExactAirportCode: airportSearchByExactAirportCode, searchAirports, searchByCityName: airportSearchByCityName, searchByAirportName: airportSearchByAirportName, airportLookup,
+    cityLookup, citySearchByCityName
 }
 
 
