a74bb4a4f0b7321f36a8fd4239c260aba0c9466b
kurt <tomashq@gmail.com>
2021-03-19 14:01:16 +0100

    coookie banner added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {
 
 import { Provider } from 'react-redux';
 import  store from './redux/store';
-
+import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom'
 import './styles/glider.scss'
 import { CookiesProvider } from 'react-cookie';
@@ -23,15 +23,23 @@ import ThemedStyleSheet from 'react-with-styles/lib/ThemedStyleSheet';
 import aphroditeInterface from 'react-with-styles-interface-aphrodite';
 import { customDatePickerTheme } from './custom-date-picker-theme';
 import ErrorCatcher from "./components/error-catcher";
+import {CookieConsentBanner} from "./components/cookie-consent-banner/cookie-consent-banner";
+import {
+    GoogleAnalyticsPageViewReporter,
+    sendWebVitalsToAnalytics
+} from "./components/cookie-consent-banner/google-analytics";
 ThemedStyleSheet.registerInterface(aphroditeInterface);
 ThemedStyleSheet.registerTheme(customDatePickerTheme());
-
+                                                                                                                                                                                            
 function Dispatcher() {
     return (
         <ErrorCatcher>
         <Provider store={store}>
-            <CookiesProvider>
+            <CookiesProvider>                                                                       
                 <Router>
+                    <GoogleAnalyticsPageViewReporter/>
+                    <CookieConsentBanner/>
+
                     <Switch>
                         <Route path="/pax/" component={DCFlightPassengersPage}/>
                         <Route path="/ancillaries/" component={DCAncillariesPage}/>
@@ -53,3 +61,9 @@ function Dispatcher() {
 }
 
 ReactDOM.render(<Dispatcher/>, document.getElementById('root'));
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(sendWebVitalsToAnalytics);
