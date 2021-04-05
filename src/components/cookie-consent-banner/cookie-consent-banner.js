229d0cf41c8c8a2c1438a47e17b0d94a3b56f500
kurt <tomashq@gmail.com>
2021-04-05 20:51:48 +0200

    banner size fixed

--- src/components/cookie-consent-banner/cookie-consent-banner.js
+++ src/components/cookie-consent-banner/cookie-consent-banner.js
@@ -25,19 +25,30 @@ export const CookieConsentBanner = () => {
         return null;
 
     return (
-        <Container className={style.cookiealert} fluid>
-        <Row className="justify-content-md-center">
-            <Col xs={6} className={style.banner}>
-                <Row>
-                <Col xs={8} className={style.cookietext}>
-                    🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪
+        <Container className={style.cookiealert}  fluid>
+            <Row>
+                <Col xs={0} md={3}>
+
                 </Col>
-                <Col xs={4} className={style.cookiebutton}>
-                    <Button variant="outline-light" onClick={onAgreeClick}>Ah, cookies... Fine</Button>
+                <Col xs={12} md={6}>
+                    <Container fluid >
+                        <Row  className="justify-content-md-center">
+                            <Col xs={12} className={style.banner} >
+                                <Row>
+                                    <Col xs={7} md={8}>
+                                        🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪 🍪
+                                    </Col>
+                                    <Col xs={5} md={4}>
+                                        <Button onClick={onAgreeClick} bsPrefix={'cookiebtn'}>Ah, cookies... Fine</Button>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row> </Container>
                 </Col>
-                </Row>
-            </Col>
-        </Row>
+                <Col xs={0} md={3}>
+
+                </Col>
+            </Row>
         </Container>
     );
 }
--- src/components/cookie-consent-banner/cookie-consent.stories.js
+++ src/components/cookie-consent-banner/cookie-consent.stories.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {CookieConsentBanner} from "./cookie-consent-banner"
-import {Button} from "react-bootstrap";
 
 export default {
     title: 'common blocks/Cookie Consent',
@@ -9,6 +8,5 @@ export default {
 
 export const Active = () => (
     <div>
-        <Button onClick={()=>console.log('clicked')}>Click me</Button>
         <CookieConsentBanner/>
     </div> )
